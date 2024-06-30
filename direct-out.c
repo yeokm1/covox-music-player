ca6ce71bc1105350bf63fdd2907143b27b384541
Yeo Kheng Meng <yeokm1@users.noreply.github.com>
2024-06-30 15:16:30 +0800

    correct formatting

--- direct-out.c
+++ direct-out.c
@@ -15,15 +15,15 @@ int main(int argc, char *argv[]){
 		return ERROR_CODE_WRONG_ARG;
 	}
 
-    char * parallelPortAddressStr = argv[1];
+	char * parallelPortAddressStr = argv[1];
 	char * valueToWriteStr = argv[2];
 
-    printf("Attempting to open parallel port at %s\n", parallelPortAddressStr);
+	printf("Attempting to open parallel port at %s\n", parallelPortAddressStr);
 
 	int parallelPortBaseAddress = (int)strtol(parallelPortAddressStr, NULL, 0);
-    int valueToWrite = (int)strtol(valueToWriteStr, NULL, 0);
+	int valueToWrite = (int)strtol(valueToWriteStr, NULL, 0);
 
-    //Unable to convert given address to hex number
+	//Unable to convert given address to hex number
 	if(parallelPortBaseAddress == 0L){
 		printf("Invalid parallel port base address.\n");
 		return ERROR_CODE_PARALLEL_ADDRESS;
@@ -34,15 +34,15 @@ int main(int argc, char *argv[]){
 		return ERROR_CODE_PARALLEL_PERMISSION;
 	}
 
-    printf("Attempting to write %d to %s\n", valueToWrite, parallelPortAddressStr);
+	printf("Attempting to write %d to %s\n", valueToWrite, parallelPortAddressStr);
 
-    outb(valueToWrite, parallelPortBaseAddress);
+	outb(valueToWrite, parallelPortBaseAddress);
 
-    //Take away permissions to access port
+	//Take away permissions to access port
 	if (ioperm(parallelPortBaseAddress, 8, 0)) {
 		printf("Error closing parallel port\n");
 		return ERROR_CODE_PARALLEL_ADDRESS;
 	}
 
-    return 0;
+	return 0;
 }
